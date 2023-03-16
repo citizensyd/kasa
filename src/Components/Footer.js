592864dc63dbaa00335e3a4a2c2ff8a8b7e1bc34
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-03-16 06:30:30 +0100

    Fix police adaptable au format  responsive sur tout le site

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,15 @@
 import React from "react";
-import LogoNoir from "../images/LOGO-noir.svg"
+import LogoNoir from "../images/LOGO-noir.svg";
 
 const Footer = (props) => {
   return (
     <footer>
-      <img src={LogoNoir} alt="logo de Kasa en blanc sur fond noir" />
-      <p className="font-weight-small-500 font-size-small-12px font-size-large-24px font-color-principal">
-        © 2020 Kaza. All rights reserved
-      </p>
+
+        <img src={LogoNoir} alt="logo de Kasa en blanc sur fond noir" />
+        <p className="font-weight-small-500 font-size-small-12px font-size-large-24px font-color-principal">
+          © 2020 Kaza. All rights reserved
+        </p>
+
     </footer>
   );
 };
