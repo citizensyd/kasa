370b1f38c72d8b0ff7dacb551d6ddd7a00a4f3f6
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-03-18 06:31:18 +0100

    Mise à jour Simplification du code css du header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 import LogoColor from "../images/LOGO-color.svg";
 
 
-const Header = (props) => {
+const Header = () => {
 
   return (
     <header>
       <img src={LogoColor} alt="Logo rose de Kasa" />
       <nav>
         <ul>
-          <li className="font-weight-small-500 font-size-small-12px font-size-large-24px font-color-primary nav-a-propos-margin">
-            <Link className="case" to="a-propos">
-              a propos
-            </Link>
-          </li>
           <li className="font-weight-small-500 font-size-small-12px font-size-large-24px font-color-primary nav-accueil-margin">
             <Link className="highlight-link case" to="/">
               accueil
+            </Link>
+          </li>
+          <li className="font-weight-small-500 font-size-small-12px font-size-large-24px font-color-primary nav-a-propos-margin">
+            <Link className="case" to="a-propos">
+              a propos
             </Link>
           </li>
         </ul>
