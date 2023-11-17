756327e874943b755eb4ef223e67ab4b7b84e2f1
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-11-17 07:08:44 +0100

    Mise à jour Modification de la gestion des erreurs dans le composant FicheLogement

--- src/pages/FicheLogement.js
+++ src/pages/FicheLogement.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { locationsFetcher } from "../Service/api.js";
+import Error from "../Components/Error";
 
 // Importation des composants Fiche et Carousel depuis le dossier Components
 import Fiche from "../Components/Fiche/Fiche";
@@ -13,39 +14,43 @@ const FicheLogement = () => {
   const { id } = useParams();
 
   // Déclaration d'une variable d'état appelée "oneLocation" avec la valeur initiale vide
-  const [oneLocation, setLocation] = useState([]);
+  const [oneLocation, setLocation] = useState(null);
+  const [noLocation, setNoLocation] = useState(false);
 
   // Utilisation de l'effet de côté pour récupérer les données de la location correspondant à l'identifiant récupéré depuis l'URL
   useEffect(() => {
     async function fetchLocation() {
       try {
         const locations = await locationsFetcher(); // Transformation des données de la réponse HTTP en objet JavaScript
-        const oneLocation = locations.filter(
-          (location) => location.id === id
-        )[0]; // Filtrage des données pour ne conserver que celles correspondant à l'identifiant de la location recherchée
-        setLocation(oneLocation); // Mise à jour de la variable d'état "oneLocation" avec les données de la location récupérées depuis le serveur
+        const oneLocation = locations.filter((location) => location.id === id)[0]; // Filtrage des données pour ne conserver que celles correspondant à l'identifiant de la location recherchée
+        if (!oneLocation) {
+          setNoLocation(true);
+        } else {
+           setNoLocation(false);
+          setLocation(oneLocation); // Mise à jour de la variable d'état "oneLocation" avec les données de la location récupérées depuis le serveur
+        }
       } catch (error) {
-        alert(
-          "Visiblement nous avons un petit soucis ! Ces articles reviennent très vite !"
-        ); // Gestion des erreurs lors de la récupération des données de la location
+        alert("Visiblement nous avons un petit soucis ! Ces articles reviennent très vite !"); // Gestion des erreurs lors de la récupération des données de la location
       }
     }
     fetchLocation();
   }, [id]); // Appel de la fonction fetchLocation une seule fois, après le premier rendu du composant, en passant un tableau vide comme deuxième argument, pour éviter qu'elle soit appelée à chaque rendu
 
-  // Affichage d'un message de chargement si les données de la location n'ont pas encore été récupérées
-  if (oneLocation.length === 0) {
-    return <div>Loading...</div>;
-  }
-
   // Rendu du composant FicheLogement avec les données de la location récupérées depuis le serveur
   return (
-    <React.Fragment>
-      <Carousel oneLocation={oneLocation} />
-      <Fiche oneLocation={oneLocation}></Fiche>
-    </React.Fragment>
+    <>
+      {noLocation ? (
+        <Error />
+      ) : oneLocation ? (
+        <>
+          <Carousel oneLocation={oneLocation} />
+          <Fiche oneLocation={oneLocation} />
+        </>
+      ) : (
+        <p>Chargement en cours...</p>
+      )}
+    </>
   );
 };
-
 // Exportation du composant FicheLogement
 export default FicheLogement;
